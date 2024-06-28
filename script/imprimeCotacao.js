1056638f02dcdc59a7646826b385a08065f90bf7
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-06-28 00:52:15 -0300

    Criando imprimeCotacao.js

---
+++ script/imprimeCotacao.js
@@ -0,0 +1,13 @@
+const lista = document.querySelector('[data-lista]');
+
+function imprimeCotacao(nome, valor) {
+  lista.innerHTML = '';
+
+  for(let multiplicador  = 1; multiplicador <= 1000; multiplicador *= 10) {
+    const listaItem = document.createElement('li');
+    listaItem.innerHTML = `${multiplicador} ${nome} R$${(multiplicador * valor).toFixed(2)}`;
+    lista.appendChild(listaItem);
+  }
+};
+
+export default imprimeCotacao
