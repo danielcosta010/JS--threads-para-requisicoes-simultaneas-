4d8618a7793232508d9f1689ed6ef7316c8ddcd3
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-06-28 02:00:12 -0300

    chamando workerDolar no script.js

--- script/script.js
+++ script/script.js
@@ -14,16 +14,6 @@ const graficoParaDolar = new Chart(graficoDolar, {
   }
 });
 
-async function conectaAPI() {
-  const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
-  const conectaTraduzido = await conecta.json();
-  const hora = geraHorario();
-  const valor = conectaTraduzido.USDBRL.ask;
-  adicionaDados(graficoParaDolar, hora, valor);
-  imprimeCotacao('Dolar', valor)
-}
-
-setInterval(() => conectaAPI(), 5000);
 
 function geraHorario() {
   const data = new Date()
@@ -32,7 +22,6 @@ function geraHorario() {
 }
 
 function adicionaDados(grafico, legenda, dados) {
-  
   grafico.data.labels.push(legenda)
   grafico.data.datasets.forEach((fiveSegundos) => {
     fiveSegundos.data.push(dados)
@@ -40,3 +29,28 @@ function adicionaDados(grafico, legenda, dados) {
   grafico.update();
 }
 
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd')
+
+workerDolar.addEventListener('message', event => {
+  const hora = geraHorario();
+  const valor = event.data.ask;
+  adicionaDados(graficoParaDolar, hora, valor)
+  imprimeCotacao('Dolar', valor)
+
+})
+
+const graficoIene = document.getElementById('graficoIene')
+
+const graficoParaIene = new Chart(graficoIene, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Iene',
+      data: [],
+      borderWidth: 1
+    }]
+  }
+
+})
